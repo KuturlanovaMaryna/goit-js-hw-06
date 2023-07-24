c6c3a30f9e7983fec26bd73d323957b04d1f62f8
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-07-24 15:09:06 +0300

    update task1 task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemEl = [];
+const listEl = document.querySelector("#ingredients");
+
+ingredients.map(ingredient => {
+  const element = document.createElement("li");
+  element.textContent = ingredient;
+  element.classList.add("item");
+
+  itemEl.push(element);  
+  listEl.append(element);
+});
