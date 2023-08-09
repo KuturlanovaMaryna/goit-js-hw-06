a22e75cdc9d0f16ee32ff55a25807727576d68b1
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-08-09 11:34:44 +0300

    update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
 const items = document.querySelectorAll('.item');
-Array.prototype.forEach.call(items, (element) => {
-    const title = element.querySelector('h2').innerHTML;
-    const itemsLength = element.querySelectorAll('li').length;
-    console.log(`Категория: ${title} \n Количество элементов: ${itemsLength}`);
+console.log(`Number of categories: ${items.length}`);
+items.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
