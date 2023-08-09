8c88b1750b9f014c86eec1f7d84355d1b34dedd8
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-08-09 12:10:52 +0300

    update task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,20 +1,17 @@
-const wrap_button = {
-  sub: document.querySelector("[data-action='increment']"),
-  add: document.querySelector("[data-action='decrement']"),
-  span: document.querySelector('#value'),
-  wrapper: document.querySelector('#counter '),
-};
 let counterValue = 0;
-const increment = () => {
-  counterValue += 1;
+const decrem = document.querySelector("button[data-action='decrement']");
+const increm = document.querySelector("button[data-action='increment']");
+const myValue = document.querySelector("#value");
 
-  document.getElementById('value').textContent = counterValue;
-};
-
-const decrement = () => {
+const countDecr = function() {
   counterValue -= 1;
-
-  document.getElementById('value').textContent = counterValue;
+  myValue.textContent = counterValue;
 };
-wrap_button.sub.addEventListener('click', increment);
-wrap_button.add.addEventListener('click', decrement);
+
+const countIncr = function() {
+  counterValue += 1;
+  myValue.textContent = counterValue;
+};
+
+decrem.addEventListener("click", countDecr);
+increm.addEventListener("click", countIncr);
